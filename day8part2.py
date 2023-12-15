c664182037fe7351714c3f439722503a62a8dc10
Monica Muranyi <monicadiana2001@yahoo.com>
2023-12-15 13:10:54 +0100

    Update day8part2.py

--- day8part2.py
+++ day8part2.py
@@ -7,6 +7,11 @@ instructions, rulesBlock = p.open("r").read().split("\n\n")
 ruleLines = rulesBlock.split("\n")
 rules = {line.split(" = ")[0]: line.split("(")[1].split(")")[0].split(", ") for line in ruleLines}
 
+# Solving this via bruteforce (i.e. simulating all steps until all destinations meet) takes too much time
+# Suppose for a starting node we reach the destination in X steps, then we also reach it in 2*X steps, 3*X steps and so on
+# That means we reach all destinations at the same time after N steps
+# where N is the least common multiple of the number of steps to reach each destionation
+
 def getStepsForStartNode(node):
     instructionIdx = 0
     steps = 0
@@ -21,4 +26,3 @@ stepsPerNode = [getStepsForStartNode(node) for node in currentNodes]
 
 print(stepsPerNode)
 print(np.lcm.reduce(stepsPerNode))
-    
